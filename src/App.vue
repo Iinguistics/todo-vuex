34de90365a8d60aa5406b8758020b3460d655631
James <Thylinguistics@gmail.com>
2021-05-02 20:28:24 -0700

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <AddTodo />
+    <FilterTodos />
+    <Todos />
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import FilterTodos from "./components/FilterTodos.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,
+    AddTodo,
+    FilterTodos
   }
 }
 </script>
@@ -21,8 +27,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  color: #fff;
+  list-style: none;
+}
+
+body{
+  background: #151515;
 }
 </style>
